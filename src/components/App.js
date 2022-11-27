c5193ee15c9e0ac0ac5f30e5c1164d588471d2e5
Oleg Andreiev <mr.olegandreev@gmail.com>
2022-11-27 02:56:24 +0200

    Add statistics component

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,15 @@
 import { Profile } from './Profile/Profile';
+import { Statistics } from './Statistics/Statistics';
 import user from '../user.json';
+import data from '../data.json';
 
 export const App = () => {
   return (
     <div>
       <Profile profile={user} />
+      <Statistics title="Upload stats" stats={data} />
+      <Statistics stats={data} />
+      <Statistics />
     </div>
   );
 };
